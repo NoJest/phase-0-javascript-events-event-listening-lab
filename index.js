0623cbb23f84a8d7d9fb275f17c7773a0b009e9b
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-09-23 09:28:38 -0400

    wondering what other things we can listen for?

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
+function addingEventListener() { 
+    const input = document.getElementById('button');
+    function mouseOverAlert(){
+        alert('Mouse is over the butt!');
+    }
+    function clickAlert() {
+      alert('heehee that tickles!');
+    }
+    
+    input.addEventListener('click', clickAlert);
+    input.addEventListener('mouseover', mouseOverAlert);
 }
+addingEventListener();
